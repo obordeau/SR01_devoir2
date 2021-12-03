4548d6b8b344b16e28f88633f4ecbc631dd7ce4b
Oceane <oceane.bordeau@etu.utc.fr>
2021-12-03 12:13:22 +0100

    ex2

---
+++ ex2.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void ecrire_fils(int nb, char *name)
+{
+    FILE *file = fopen(name, "w");
+    if (file == NULL)
+    {
+        printf("Le fichier n'a pas pu être ouvert\n");
+        exit(1);
+    }
+    if (fputc(nb, file) == EOF)
+    {
+        printf("Erreur lors de l'écriture d'un caractère\n");
+        exit(1);
+    }
+    if (fclose(file) == EOF)
+    {
+        printf("Erreur lors de la fermeture du flux\n");
+        exit(1);
+    }
+    exit(0);
+}
+
+void lire_pere(int *nb, char *name)
+{
+    FILE *file = fopen(name, "r");
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "Le fichier n'a pas pu être ouvert\n");
+        exit(1);
+    }
+    int n = fgetc(file);
+    if (n != EOF)
+    {
+        *nb = n;
+    }
+    if (fclose(file) == EOF)
+    {
+        fprintf(stderr, "Erreur lors de la fermeture du flux\n");
+        exit(1);
+    }
+    if (remove(name) == 0)
+    {
+        printf(" Le fichier a été détruit avec succès.\n");
+    }
+    else
+    {
+        printf("Impossible de détruire le fichier\n");
+        perror("Erreur");
+        exit(1);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    pid_t pid = fork();
+    char fichier[] = "fichier";
+    int status;
+    switch (pid)
+    {
+    case -1:
+        printf("Erreur fork\n");
+        break;
+    case 0:
+        printf("Je suis le fils\n");
+        int n = 5;
+        printf(" Je vais écrire %d dans le fichier %s\n", n, fichier);
+        ecrire_fils(n, fichier);
+        break;
+    default:
+        printf("Je suis le père\n");
+        printf(" Je vais récupérer le chiffre écrit par mon fils dans le fichier %s\n", fichier);
+        wait(&status);
+        if (status == 0)
+        {
+            int *nb = (int *)malloc(sizeof(int));
+            lire_pere(nb, fichier);
+            printf("Le fils avait écrit %d dans le fichier\n", *nb);
+        }
+        break;
+    }
+    return 0;
+}
