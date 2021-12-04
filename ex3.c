33824c45e9b4e0d9a7a093c012d3c24547eb9018
Martin Schneider <martin.schneider2001@gmail.com>
2021-12-04 17:07:50 +0100

    Ex3 working a cleaner

--- ex3.c
+++ ex3.c
@@ -6,48 +6,41 @@
 #include <time.h>
 #include <string.h>
 
+#define LEN 150
 #define MAX 100
 #define SEUIL 3
 
-void ecrire_fils(int nb, char *name)
+int ecrire_fils(int nb, char *name)
 {
     FILE *file = fopen(name, "w");
     if (file == NULL)
     {
-        printf("Le fichier n'a pas pu être ouvert\n");
-        exit(1);
-    }
-    else
-    {
-        printf("Fichier ouvert avec succès\n");
+        return EXIT_FAILURE;
     }
     if (fputc(nb, file) == EOF)
     {
-        printf("Erreur lors de l'écriture d'un caractère\n");
-        exit(1);
+        return EXIT_FAILURE;
     }
     if (fclose(file) == 0)
     {
-        printf("Fichier fermé\n");
-        exit(0);
+        return EXIT_SUCCESS;
     }
-    if (fclose(file) == EOF)
+    else
     {
-        printf("Erreur lors de la fermeture du flux\n");
-        exit(1);
+        return EXIT_FAILURE;
     }
 
-    exit(0);
+    return EXIT_SUCCESS;
 }
 
-void lire_pere(int *nb, char *name)
+int lire_pere(int *nb, char *name)
 {
     FILE *file = fopen(name, "r");
 
     if (file == NULL)
     {
         fprintf(stderr, "Le fichier n'a pas pu être ouvert\n");
-        exit(1);
+        return 1;
     }
     else
     {
@@ -58,14 +51,10 @@ void lire_pere(int *nb, char *name)
     {
         *nb = n;
     }
-    if (fclose(file) == 0)
-    {
-        printf("Fichier fermé\n");
-    }
     if (fclose(file) == EOF)
     {
         fprintf(stderr, "Erreur lors de la fermeture du flux\n");
-        exit(1);
+        return 1;
     }
     if (remove(name) == 0)
     {
@@ -75,7 +64,7 @@ void lire_pere(int *nb, char *name)
     {
         printf("Impossible de détruire le fichier\n");
         perror("Erreur");
-        exit(1);
+        return 1;
     }
 }
 
@@ -105,35 +94,53 @@ int find_max(int *tab, int debut, int fin)
 {
     if ((fin - debut + 1) > SEUIL)
     {
-        int status;
+        int status1, status2;
         int max_debut = tab[debut];
         int max_fin = tab[fin];
 
-        printf("On sépare le tableau\n");
+        printf("Création de deux processus\n");
         pid_t pid1 = fork();
         if (pid1 == 0)
         {
             printf("Ici le fils 1 %d, tableau a evaluer : [%d:%d]\n", getpid(), debut, (debut + fin) / 2);
-            ecrire_fils((int)find_max(tab, debut, (debut + fin) / 2), "fichier1");
+            printf("Fils 1 %d stop\n", getpid());
+            kill(getpid(), SIGSTOP);
+            printf("Reprise fils 1 %d\n", getpid());
+            int local_max = find_max(tab, debut, (debut + fin) / 2);
+            int ctrl = ecrire_fils(local_max, "fichier1");
+            if (ctrl == 0)
+            {
+                printf("Ecriture fichier 1 ok\n");
+            }
             exit(EXIT_SUCCESS);
         }
         pid_t pid2 = fork();
         if (pid2 == 0)
         {
             printf("Ici le fils 2 %d, tableau a evaluer : [%d:%d]\n", getpid(), ((debut + fin) / 2) + 1, fin);
-            ecrire_fils((int)find_max(tab, ((debut + fin) / 2) + 1, fin), "fichier2");
+            int local_max = find_max(tab, ((debut + fin) / 2) + 1, fin);
+            int ctrl = ecrire_fils(local_max, "fichier2");
+            if (ctrl == 0)
+            {
+                printf("Ecriture fichier 2 ok\n");
+            }
             exit(EXIT_SUCCESS);
         }
         else
         {
-            waitpid(pid1, NULL, 0);
-            waitpid(pid2, NULL, 0);
-            int max_debut;
-            lire_pere(&max_debut, "fichier1");
-            printf("MAX DEBUT : %d\n", max_debut);
+            waitpid(pid2, &status1, 0);
             int max_fin;
             lire_pere(&max_fin, "fichier2");
             printf("MAX FIN : %d\n", max_fin);
+            printf("Fin fils2 %d\n", pid2);
+            printf("fils 1 %d back\n", pid1);
+            kill(pid1, SIGCONT);
+            waitpid(pid1, &status2, 0);
+            printf("Fin fils1 %d\n", pid1);
+            int max_debut;
+            lire_pere(&max_debut, "fichier1");
+            printf("MAX DEBUT : %d\n", max_debut);
+
             printf("Le max du debut est %d et de fin est %d pour le tableau [%d : %d]\n", max_debut, max_fin, debut, fin);
             return maxi(max_debut, max_fin);
         }
@@ -152,12 +159,12 @@ int random_int()
 int main()
 {
     srand(time(NULL));
-    int tab[5];
-    for (int i = 0; i < 5; i++)
+    int tab[LEN];
+    for (int i = 0; i < LEN; i++)
     {
         tab[i] = random_int();
         printf("%d\n", tab[i]);
     }
-    printf("%d\n", find_max(tab, 0, 4));
+    printf("\nMaximum global du tableau : %d\n", find_max(tab, 0, LEN - 1));
     return 0;
 }
